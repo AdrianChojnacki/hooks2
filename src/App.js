b5e3fb01ef960b777cad516049d4930dbbbb35ca
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-10-09 23:04:33 +0200

    hooks 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Item from "./Item";
 
-function App() {
+export default function App() {
+  const [items] = React.useState({
+    items1: "ogórki",
+    items2: "sok",
+    items3: "coś do picia",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lista zakupów</h1>
+      <ul>
+        <Item name={items.items1} example={2} />
+        <Item name={items.items2} example={1} />
+        <Item name={items.items3} example={3} />
+      </ul>
+    </>
   );
 }
-
-export default App;
